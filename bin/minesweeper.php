d1e8bf0a6f954cda114ea10bc351bcc23b60bad5
artemaaaaaaaaaaaaaaaaaaa <artm-grachev@mail.ru>
2025-09-16 01:25:50 +0300

    Fix autoload for global and local usage

--- bin/minesweeper.php
+++ bin/minesweeper.php
@@ -1,21 +1,7 @@
 #!/usr/bin/env php
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
-// Ищем автозагрузчик Composer
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    // локальная установка
-    require __DIR__ . '/../vendor/autoload.php';
-} elseif (file_exists($autoload = getenv('COMPOSER_HOME') . '/vendor/autoload.php')) {
-    // глобальная установка
-    require $autoload;
-} else {
-    // fallback, если автозагрузчик не найден
-    fwrite(STDERR, "Autoload file not found. Run 'composer install' first.\n");
-    exit(1);
-}
-
-// подключаем функцию из неймспейса
 use function Artyo\Minesweeper\Controller\startGame;
 
-// запускаем игру
 startGame();
