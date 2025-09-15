a29963b7615308bdb13c0c67a212b47bdc039c79
artemaaaaaaaaaaaaaaaaaaa <artm-grachev@mail.ru>
2025-09-16 02:17:16 +0300

    Fix autoload for global and local usage

--- bin/minesweeper.php
+++ bin/minesweeper.php
@@ -3,7 +3,7 @@
 
 $autoload = __DIR__ . '/../vendor/autoload.php';
 if (!file_exists($autoload)) {
-    $autoload = dirname(__DIR__, 2) . '/autoload.php'; 
+    $autoload = dirname(__DIR__, 2) . '/autoload.php';
 }
 require $autoload;
 
