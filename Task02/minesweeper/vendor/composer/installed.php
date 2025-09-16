b93b695735e38ddf27f8ccac939d2f2121100a16
artemaaaaaaaaaaaaaaaaaaa <artm-grachev@mail.ru>
2025-09-16 03:05:14 +0300

    Task02

---
+++ Task02/minesweeper/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'artyo/minesweeper',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c4b2de4c3270990b941999c61a161ac9f730feac',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'artyo/minesweeper' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c4b2de4c3270990b941999c61a161ac9f730feac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.6',
+            'version' => '0.12.6.0',
+            'reference' => 'f12b650d3738e471baed6dd47982d53c5c0ab1c3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
